5c857f336005c7755efaf2f788ff639a910608f5
Ichters Emma <ichters.emma@gmail.com>
2017-01-12 11:40:48 +0100

    MAJ

--- app/controller/list_controller.js
+++ app/controller/list_controller.js
@@ -1,20 +1,18 @@
-angular.module("todo").controller(
-  "ListController",
-  [
-    "$scope", "$http", "List",
+angular.module("todo").controller("ListController",["$scope", "$http", "List",
     function($scope, $http, List){
-      $http.get("http://todos.api.netlor.fr/lists",
-      {headers: {"Authorization": "Token token=47244e6526354e15a3b3f9386de73d24"}})
-      .then(function($response){
-        // $scope.lists = [];
-        // $response.data.forEach(function(data){
-        //   var newList = new List(data);
-        //   $scope.lists.push(newList);
-        // });
-      },
-      function(error){
-        console.log(error);
-      });
+      $http.get("http://todos.api.netlor.fr/lists",{
+            headers: {
+                "Authorization": "Token token=47244e6526354e15a3b3f9386de73d24"
+            }
+        }).then(function(response){
+            console.log(response);
+            $scope.lists = [];
+            response.data.forEach(function(data){
+                var newList = new List(data);
+                $scope.lists.push(newList);
+            });
+        },function(error){
+            console.log(error);
+        });
     }
-  ]
-);
+]);
